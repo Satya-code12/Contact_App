4faba61eb1064fa871c28262e79e5f7a9e2e329c
Satyabrata Ojha <satyabrat637@gmail.com>
2024-08-05 17:01:50 +0530

    Initial commit

---
+++ ContextApi/src/Components/Login.jsx
@@ -0,0 +1,26 @@
+import React, {useState, useContext} from 'react'
+import UserContext from "../Context/UserContext"
+
+function Login() {
+
+  const [username, setUsername] = useState("")
+  const [passwd, setPasswd] = useState("")
+
+  const {setUser} = useContext(UserContext)
+
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    setUser({username, passwd})
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}  placeholder='username' /> {" "}
+      <input type="text" value={passwd} onChange={(e) => setPasswd(e.target.value)}  placeholder='password' />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ ContextApi/src/Context/UserContextProvider.jsx
@@ -0,0 +1,18 @@
+//Context data refers to any piece of information or state that we want to make available to multiple components in a React aplication.
+
+
+import React from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({children}) =>{
+  const [user, setUser] = React.useState(null)
+   return(
+
+    // the provider component is responsible for making the context data available to any component that it wraps around
+
+    <UserContext.Provider value={{user, setUser}}>
+      {children}
+    </UserContext.Provider>
+   )
+}
+export default UserContextProvider;
